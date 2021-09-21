68cf4608a08c66b138467292d2d44a8f241f4995
Alexander Watson <zredlined@users.noreply.github.com>
2021-09-21 13:43:42 +0000

    initial checkin

---
+++ download.sh
@@ -0,0 +1,14 @@
+# Following links are from https://datadryad.org/stash/dataset/doi:10.5061/dryad.2rs41
+WORKING_DIR=mice_data_set
+
+# Download experiment datasets
+mkdir -p $WORKING_DIR/data $WORKING_DIR/out $WORKING_DIR/gemma/output
+wget -O $WORKING_DIR/data/geno.txt.gz https://datadryad.org/stash/downloads/file_stream/4344
+gunzip -f $WORKING_DIR/data/geno.txt.gz
+wget -O $WORKING_DIR/data/map.txt https://datadryad.org/stash/downloads/file_stream/4342
+wget -O $WORKING_DIR/data/pheno.csv https://datadryad.org/stash/downloads/file_stream/4340
+
+# Download GEMMA
+wget https://github.com/genetics-statistics/GEMMA/releases/download/v0.98.4/gemma-0.98.4-linux-static-AMD64.gz -P $WORKING_DIR/
+gunzip -f $WORKING_DIR/gemma-0.98.4-linux-static-AMD64.gz
+chmod u+x $WORKING_DIR/gemma-0.98.4-linux-static-AMD64
